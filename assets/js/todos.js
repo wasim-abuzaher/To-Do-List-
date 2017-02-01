b7e649d330cc569bf41fe907cfe0cfb4c64af6aa
wasim-abuzaher <wasim.abuzaher@gmail.com>
2017-01-31 21:05:41 -0500

    Improved functionality and design

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,24 @@
-//Check off todos by clicking on them
-$("li").on("click", function(){
-	if($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	} else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
+//Create todos by pressing Enter key
+$("input[type='text']").on("keypress", function(e){
+	if(e.which === 13){
+		//grab text from input
+		var todoText = $(this).val();
+		//create new li and append to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		//clear text input
+		$(this).val("");
 	}
+});
+
+//Check off todos by clicking on them
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Clicking "X" delete the todos
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
 });
